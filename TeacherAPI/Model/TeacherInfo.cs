c6f98c549180cca8cadcab5df7a1570f5712d8d3
Mubin Codemen <codemen.almubin@gmail.com>
2023-10-30 22:50:11 +0600

    Model added

--- TeacherAPI/Model/TeacherInfo.cs
+++ TeacherAPI/Model/TeacherInfo.cs
@@ -1,16 +1,15 @@
-﻿namespace StudentAPI.Model
+﻿namespace TeacherAPI.Model
 {
-    public class Student
+    public class TeacherInfo
     {
-        public int StudentId { get; set; } 
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
 
-        
     }
 }
